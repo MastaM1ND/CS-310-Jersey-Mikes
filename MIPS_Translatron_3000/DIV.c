4d4bd39a2cb6cb6ca246c6f9d3d628bf98774ad7
MastaM1ND <owenmbarry@gmail.com>
2025-04-03 19:23:44 -0400

    LW and DIV comments and bugs fixed

--- MIPS_Translatron_3000/DIV.c
+++ MIPS_Translatron_3000/DIV.c
@@ -8,49 +8,84 @@
 #include "Instruction.h"
 
 void div_reg_assm(void) {
+	// Checking that the op code matches
+		// strcmp(string1, string2) return 0 if they match
 	if (strcmp(OP_CODE, "DIV") != 0) {
+		// If the op code doesnt match, this isnt the correct command
 		state = WRONG_COMMAND;
 		return;
 	}
 
-	// check par
+	/*
+		Checking the type of parameters
+	*/
+
+	// The first parameter should be a register
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+	// Param 2 needs to be a register
 	if (PARAM2.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
+
+	/*
+		Checking the value of parameters
+	*/
+
+	// Rt should be 31 or less
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
+
+	// Rs should be 31 or less
 	if (PARAM2.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
 
-	// create binary
+	/*
+		Putting the binary together
+	*/
+
+	// Set the opcode
 	setBits_str(31, "000000");
+	//set rt
 	setBits_num(20, PARAM1.value, 5);
+	// set rs
 	setBits_num(25, PARAM2.value, 5);
+	// set funct
 	setBits_str(5, "011010");
+	//set offset
 	setBits_num(15, 0, 10);
 	
 	state = COMPLETE_ENCODE;
 }
 
 void div_reg_bin(void) {
+	/*
+        Checking opcode (bits 31..26) and funct (bits 5..0)
+    */
 	if (checkBits(31, "000000") != 0 || checkBits(5, "011010") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
-	// find binary
+	/*
+        Extracting register fields
+    */
 	uint32_t Rs = getBits(25, 5);
 	uint32_t Rt = getBits(20, 5);
 
+	/*
+        Setting instruction values
+        The DIV instruction does not have a destination register (Rd)
+        The DIV Syntax is DIV Rs, Rt
+        The result is stored in the HI and LO registers
+    */
 	setOp("DIV");
 	setParam(2, REGISTER, Rs);
 	setParam(1, REGISTER, Rt);
--- MIPS_Translatron_3000/LW.c
+++ MIPS_Translatron_3000/LW.c
@@ -1,71 +1,116 @@
+/*
+* Author: Ol' Jim
+* Date: 06/13/2012
+* ByteForge Systems
+* MIPS-Translatron 3000
+*/
+
 #include "Instruction.h"
 
 void lw_immd_assm(void) {
+	// Checking that the op code matches
+		// strcmp(string1, string2) returns 0 if they match
 	if (strcmp(OP_CODE, "LW") != 0) {
-
+		// If the op code doesn't match, this isn't the correct command
 		state = WRONG_COMMAND;
 		return;
 	}
-	
-	
+
+	/*
+		Checking the type of parameters
+	*/
+
+	// The first parameter should be a register
 	if (PARAM1.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
 
+	// The second parameter should be an immediate
 	if (PARAM2.type != IMMEDIATE) {
 		state = INVALID_IMMED;
 		return;
 	}
 
-	//reg
+	// The third parameter should be a register
 	if (PARAM3.type != REGISTER) {
 		state = MISSING_REG;
 		return;
 	}
 
-	//check if register 1 and 3
+	/*
+		Checking the value of parameters
+	*/
+
+	// Rt should be 31 or less
 	if (PARAM1.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
-	if ( PARAM3.value > 31) {
+
+	// Rs should be 31 or less
+	if (PARAM3.value > 31) {
 		state = INVALID_REG;
 		return;
 	}
-	//register 2 
-	if ( PARAM2.value > 0x7FFF) {
+
+	// The immediate value is limited to 16 bits, this is 0xFFFF
+	if (PARAM2.value > 0xFFFF) {
 		state = INVALID_IMMED;
 		return;
 	}
-	
-	//encode instruction 
+
+	/*
+		Putting the binary together
+	*/
+
+	// Set the opcode
 	setBits_str(31, "100011");
+
+	// Set Rt
 	setBits_num(20, PARAM1.value, 5);
-	setBits_num(25, PARAM3.value, 5);
+
+	// Set immediate value
 	setBits_num(15, PARAM2.value, 16);
 
+	// Set Rs
+	setBits_num(25, PARAM3.value, 5);
 
+	// Tell the system the encoding is done
 	state = COMPLETE_ENCODE;
 }
 
-
 void lw_immd_bin(void) {
-
-	//check op code 
+	// Check if the op code bits match
+		// checkBits(start_bit, bit_string) returns 0 if the bit_string matches
+		// Any x will be skipped
+		// If the manual shows (0), then the value of that bit doesn't matter
 	if (checkBits(31, "100011") != 0) {
 		state = WRONG_COMMAND;
 		return;
 	}
 
+	// If the op code bits match, then the rest can be read as correctly
+
+	/*
+		Finding values in the binary
+	*/
+
+	// getBits(start_bit, width)
 	uint32_t Rs = getBits(25, 5);
 	uint32_t Rt = getBits(20, 5);
 	uint32_t imm16 = getBits(15, 16);
 
-	setOp("LW");
-	setParam(1, REGISTER, Rt);
-	setParam(3, REGISTER, Rs);
-	setParam(2, IMMEDIATE, imm16);
-	state = COMPLETE_DECODE;
+	/*
+		Setting Instruction values
+	*/
 
+	setOp("LW");
+	// setParam(param_num, param_type, param_value)
+	setParam(1, REGISTER, Rt); // destination
+	setParam(2, IMMEDIATE, imm16); // immediate operand
+	setParam(3, REGISTER, Rs); // source register operand
+
+	// Tell the system the decoding is done
+	state = COMPLETE_DECODE;
 }
